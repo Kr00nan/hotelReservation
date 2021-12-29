77df36ae4e72060bd8b3a172aaa3a16ea654455f
Michael Kroon <kroonology@gmail.com>
2021-12-29 16:10:47 -0700

    new model tester class, improved testing of model classes

--- src/model/Tester.java
+++ src/model/Tester.java
@@ -5,14 +5,15 @@ import java.util.*;
 public class Tester {
     public static void main(String[] args) {
         Customer customer = new Customer("first", "second", "j@domain.com");
-        System.out.println(customer);
+//        System.out.println(customer);
 
         Room room = new Room("100", 135.00, RoomType.DOUBLE);
-        System.out.println(room);
+//        System.out.println(room);
 
         Calendar cal = Calendar.getInstance();
         cal.set(2004, 0, 7, 15, 40);
-        Reservation reservation = new Reservation(customer, room, );
+        Reservation reservation = new Reservation(customer, room, new Date(), new Date());
+        System.out.println(reservation);
 
     }
 }
