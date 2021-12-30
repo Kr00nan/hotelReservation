dd8626d0ba63bd20264b2a11f6e1353bb7a2dd91
Michael Kroon <kroonology@gmail.com>
2021-12-30 16:05:33 -0700

    finalize tests for model classes

--- src/model/Tester.java
+++ src/model/Tester.java
@@ -5,10 +5,10 @@ import java.util.*;
 public class Tester {
     public static void main(String[] args) {
         Customer customer = new Customer("Michael", "Kroon", "michael@kroon.com");
-//        System.out.println(customer);
+        System.out.println(customer);
 
         Room room = new Room("100", 135.00, RoomType.DOUBLE);
-//        System.out.println(room);
+        System.out.println(room);
 
         Reservation reservation = new Reservation(customer, room, new Date(), new Date());
         System.out.println(reservation);
