4c3464e558bf119ba362de6c4ce3f3f003e7543d
Michael Kroon <kroonology@gmail.com>
2021-12-30 16:08:04 -0700

    flesh out ReservationService class

--- src/service/ReservationService.java
+++ src/service/ReservationService.java
@@ -8,26 +8,59 @@ import java.util.LinkedList;
 
 public class ReservationService {
 
-    private static Collection<IRoom> reservations = new LinkedList<>();
+    private static Collection<IRoom> rooms = new LinkedList<>();
+    private static Collection<Reservation> reservations = new LinkedList<>();
 
     public void addRoom(Room room){
-        //TODO
+        rooms.add(room);
     }
 
     public static IRoom getARoom(String roomID){
-        //TODO
+        IRoom room = null;
+        try {
+            for(IRoom theOne : rooms) {
+                if (theOne.getRoomNumber().equals(roomID)) {
+                    room = theOne;
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("That room does not exist.");
+        }
+
+        return room;
     }
 
     public static Reservation reserveARoom(Customer customer, IRoom room, Date checkInDate, Date checkOutDate){
-        //TODO
+        Reservation reservation = new Reservation(customer, room, checkInDate, checkOutDate);
+        reservations.add(reservation);
+        return reservation;
     }
 
     public static Collection<IRoom> findRooms(Date checkInDate, Date checkOutDate){
-        //TODO
+        Collection<IRoom> availableRooms = new LinkedList<>();
+        try {
+            // what rooms are available during the provided date range
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("There are no rooms available with those dates");
+        }
+        return availableRooms;
     }
 
     public static Collection<Reservation> getCustomerReservations(Customer customer){
-        //TODO
+        Collection customerReservations = new LinkedList();
+        try {
+            for(Reservation reservation : reservations) {
+                if (reservation.getCustomer().equals(customer)) {
+                    customerReservations.add(reservation);
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("There are no reservations for this customer.");
+        }
+        return customerReservations;
     }
 
     public static void printAllReservations(){
