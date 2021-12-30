e8e43a840dd1c7ddd99b801108e155ac3a6a0005
Michael Kroon <kroonology@gmail.com>
2021-12-30 10:52:17 -0700

    move email verification logic from CustomerService to HotelResource

--- src/api/HotelResource.java
+++ src/api/HotelResource.java
@@ -1,15 +1,17 @@
 package api;
 
 import model.Customer;
+import model.IRoom;
 import model.Reservation;
 import model.Room;
+import service.CustomerService;
 
 import static service.CustomerService.*;
 import static service.ReservationService.*;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.regex.Pattern;
 
 public class HotelResource {
     public static Customer retrieveCustomer(String email){
@@ -17,23 +19,31 @@ public class HotelResource {
     }
 
     public static void createACustomer(String firstName, String lastName, String email) {
-        addCustomer(firstName, lastName, email);
+        //TODO
+        String emailRegex = "^(.+)@(.+).(.+)$";
+        Pattern pattern = Pattern.compile(emailRegex);
+
+        // check to make sure email format is legit
+        if (pattern.matcher(email).matches()) {
+            CustomerService.addCustomer(firstName, lastName, email);
+        } else {
+            throw new IllegalArgumentException("Email format incorrect; required format: xxx@xxx.xxx");
+        }
     }
 
-//    public static Room getRoom(String roomNumber){
-//        return getARoom(roomNumber);
-//    }
+    public static IRoom getRoom(String roomNumber){
+        //TODO
+    }
 
     public static Collection<Room> findARoom(Date checkIn, Date checkOut) {
-        Collection<Room> rooms = new ArrayList<>();
-        return rooms;
+        //TODO
     }
 
-//    public static Reservation bookARoom(String customerEmail, Room room, Date checkInDate, Date checkOutDate){
-//        Customer customer = getCustomer(customerEmail);
-//        Reservation reservation = reserveARoom(customer, room, checkInDate, checkOutDate);
-//        return reservation;
-//    }
+    public static Reservation bookARoom(String customerEmail, Room room, Date checkInDate, Date checkOutDate){
+        Customer customer = getCustomer(customerEmail);
+        Reservation reservation = reserveARoom(customer, room, checkInDate, checkOutDate);
+        return reservation;
+    }
 
     public static Collection<Reservation> retrieveCustomersReservations(String customerEmail){
         Customer customer = retrieveCustomer(customerEmail);
--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -1,35 +1,24 @@
 package service;
 
 import model.Customer;
-
 import java.util.*;
-import java.util.regex.Pattern;
 
 public class CustomerService {
 
     private static Collection<Customer> customers = new LinkedList<>();
 
-    public static void addCustomer(String email, String firstName, String lastName){
-
-        String emailRegex = "^(.+)@(.+).(.+)$";
-        Pattern pattern = Pattern.compile(emailRegex);
-
-        // check to make sure email format is legit
-        if (pattern.matcher(email).matches()) {
-            customers.add(new Customer(firstName, lastName, email));
-        } else {
-            throw new IllegalArgumentException("Email format incorrect; required format: xxx@xxx.xxx");
-        }
+    public static void addCustomer(String firstName, String lastName, String email){
+        customers.add(new Customer(firstName, lastName, email));
     }
 
     public static Customer getCustomer(String customerEmail){
-        Customer retval = null;
-        for(Customer customer : customers) {
-            if (customer.getEmail().equals(customerEmail)) {
-                retval = customer;
+        Customer customer = null;
+        for(Customer theOne : customers) {
+            if (theOne.getEmail().equals(customerEmail)) {
+                customer = theOne;
             }
         }
-        return retval;
+        return customer;
     }
 
     public static Collection<Customer> getAllCustomers(){
